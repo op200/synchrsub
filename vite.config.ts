f216fef60563d4fa7bb9e0ef8efb27110006d0e0
op200 <3205845543@qq.com>
2025-01-21 06:30:18 +0800

    add url base

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/synchrsub/' : '/', // 添加 base 配置
 })
